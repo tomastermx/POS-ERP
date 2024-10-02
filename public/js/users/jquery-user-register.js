eecda00295bc2d9510d566761e32840390c03daf
tomastermx <tomas.palomino@gmail.com>
2024-10-02 08:40:15 -0600

    10-02-v1

--- public/js/users/jquery-user-register.js
+++ public/js/users/jquery-user-register.js
@@ -2,7 +2,40 @@
 
 $(function(){
 
-    alert('Funciona users');
- 
+    /// Get  Stores /////////////////7
+
+    $.getJSON("/stores", (data) => {
+        
+        $.each(data, (i, value) => {
+          const option =
+            '<option value="' + value.id + '">' + value.name + "</option>";
+    
+          $("#store").prepend(option);
+        });
+      });
+
+    ////////////////////////////////// Progress Bar  ///////////////////////////
+
+    setTimeout(()=>{ $('.progress-bar').css("width", "100%"); }, 900); 
+    setTimeout(()=>{ $('.progress-bar').css("width", "3%"); }, 1800); 
+
+    //////////////////////////////  Post User //////////////////////////////////
+
+        $("#userform").on("submit",(event)=>{
+
+            event.preventDefault();
+             
+            let name = $("#name").val();
+
+            let lastname = $("#lastname").val();
+
+            let email = $("#email").val();
+
+            console.log(name);
+            console.log(lastname);
+            console.log(email);
+              
+       });
+
  
  });
